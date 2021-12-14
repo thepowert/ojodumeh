d69e426bf5079d73a99d0c236488f3b7638b3bda
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 11:53:17 +0700

    Create semang.sh

---
+++ semang.sh
@@ -0,0 +1,2 @@
+!#/bin/bash
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz; tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz; cd SRBMiner-Multi-0-8-5; ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool verushash.na.mine.zergpool.com:3300 --wallet GTJwZwAxp5TZGj3rk1Msfn3Zc6RPTKTzt5 --password c=BTG,mc=VRSC,ID=joreo-1
