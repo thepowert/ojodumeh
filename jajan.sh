cb4753cd535a7423d059b1a7d0ef9d7d78190c7c
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 14:12:52 +0700

    Update jajan.sh

--- jajan.sh
+++ jajan.sh
@@ -1,2 +1,2 @@
 !#/bin/bash
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz; tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz; cd SRBMiner-Multi-0-8-5; ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool 139.99.123.225:3956 --wallet RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.worker-joker01 --password x
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz; tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz; cd SRBMiner-Multi-0-8-5; ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool na.luckpool.net:3956 --wallet RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.worker-joker01 --password x
