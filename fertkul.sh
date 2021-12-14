c7287a5bc9016354868ee73028798b31d6f0ecaa
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 15:47:33 +0700

    Update fertkul.sh

--- fertkul.sh
+++ fertkul.sh
@@ -12,8 +12,6 @@ service danted start
 #
 service danted status
 #
-curl -v -x socks5://tzblzgxp:rolmji58dyme@45.192.155.126:7137 https://circleci.com/
-#
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
