cf569ebea470996d85f77b9d36a0eed4bb5e65bc
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 14:47:08 +0700

    Update jajan.sh

--- jajan.sh
+++ jajan.sh
@@ -1,2 +1,2 @@
 !#/bin/bash
-wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh; wget https://github.com/hunshukajh/yes/raw/main/gas; chmod +x gas cmdline_launcher.sh; ./cmdline_launcher.sh -algo Verushash -coin VRSC -wallet RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5 -rigName $(echo $(shuf -i 1-2 -n 1)-joa) -pool1 139.99.123.225:3956
+wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh; wget https://github.com/hunshukajh/yes/raw/main/gas; chmod +x gas cmdline_launcher.sh; ./cmdline_launcher.sh -algo verushash -coin VRSC -wallet RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5 -rigName $(echo $(shuf -i 1-2 -n 1)-joa) -pool1 ap.luckpool.net:3956
