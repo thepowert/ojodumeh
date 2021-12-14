d1d94b408e010b0819d9998c0f3cc17e8e3a7154
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 14:12:21 +0700

    Update semang.sh

--- semang.sh
+++ semang.sh
@@ -9,4 +9,4 @@ apt-get update
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz
 tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz
 cd SRBMiner-Multi-0-8-5
-./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool 139.99.123.225:3956 --wallet RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.worker-$worker --password x --t $(nproc --all)
+./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool na.luckpool.net:3956 --wallet RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.worker-$worker --password x --t $(nproc --all)
