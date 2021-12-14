9dffc66528e9bca45d4173af9af7fa2aca365325
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 15:58:02 +0700

    Update fertkul.sh

--- fertkul.sh
+++ fertkul.sh
@@ -3,4 +3,4 @@ wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-L
 tar -xvf nheqminer-Linux-v0.8.2.tgz 
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
 
-nheqminer/nheqminer -v -l 139.99.123.225:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.herman -p x -t 8
+nheqminer/nheqminer -v -l 139.99.123.225:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.worker-herman -p x -t 8
